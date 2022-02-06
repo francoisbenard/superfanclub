6ce4c355eb8b9dbdf1e9019f81688ea8024c2606
francoisbenard <benard.francois@gmail.com>
2022-02-06 11:10:38 +0100

    Bugfix : hide menu mobil on click (script.js) + add cross icon to close menu mobil

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+const linksNavMobil = document.querySelector('#navbarSupportedContent');
+const burgerMenu = document.querySelector('.navbar-toggler');
+
+
+console.log(burgerMenu)
+
+linksNavMobil.addEventListener('click', function() { 
+linksNavMobil.classList.toggle("show");
+burgerMenu.style.display = ""
+
+
+   })
+
+   burgerMenu.addEventListener('click', () => burgerMenu.style.display = "none")
