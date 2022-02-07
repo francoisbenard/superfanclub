6abe569545c60bfe7412b4f3d5126ae8486b3aa5
francoisbenard <benard.francois@gmail.com>
2022-02-07 19:33:37 +0100

    bugfix img on cards and grow font-size on menu mobil

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,13 @@
+// hide and show menu mobil and icon
+
 const linksNavMobil = document.querySelector('#navbarSupportedContent');
 const burgerMenu = document.querySelector('.navbar-toggler');
 
-
 console.log(burgerMenu)
 
-linksNavMobil.addEventListener('click', function() { 
-linksNavMobil.classList.toggle("show");
-burgerMenu.style.display = ""
+linksNavMobil.addEventListener('click', function () {
+   linksNavMobil.classList.toggle("show");
+   burgerMenu.style.display = ""
+})
 
-
-   })
-
-   burgerMenu.addEventListener('click', () => burgerMenu.style.display = "none")
+burgerMenu.addEventListener('click', () => burgerMenu.style.display = "none")
